c42f067bcdc03c26fadb6d915dd76f82a786941a
yp <w101379@163.com>
2018-06-22 23:38:46 +0800

    detail
    detail

--- wechatprogrammme/pages/weekly/weekly.js
+++ wechatprogrammme/pages/weekly/weekly.js
@@ -4,19 +4,22 @@ Page({
       name:"教父",
       comment:"最精彩的黑帮电影",
       imagepath:"/images/godfather.jpg",
-      isHighlyRecommended:true
+      isHighlyRecommended:true,
+      id:77
     },
       {
         name: "泰塔尼克号",
         comment: "失去的才是永恒",
         imagepath: "/images/titanic.jpg",
-        isHighlyRecommended: false
+        isHighlyRecommended: false,
+        id:88
       },
       {
         name: "这个杀手不太冷",
         comment: "小萝莉与大叔的爱情故事",
         imagepath: "/images/killer.jpg",
-        isHighlyRecommended: false
+        isHighlyRecommended: false,
+        id:99
       }
     ],
     count:123,
@@ -33,5 +36,13 @@ Page({
     this.setData({
       currentpage: this.data.weeklymovielist.length - 1
     })
+  },
+
+  f1:function(event){
+     var movieID=event.currentTarget.dataset.movieId;
+     console.log(movieID);
+     wx.navigateTo({
+       url: '/pages/detail/detail?id='+movieID
+     })
   }
 })
